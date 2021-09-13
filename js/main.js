5c323c90dbf19bbe111671e98d881a8b74ef64ea
Latif Basham <latifbasham@Latifs-MacBook-Pro.local>
2021-09-13 15:30:01 -0700

    my car

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-/*----- constants -----*/
+//*----- constants -----*//
 
 
 
@@ -9,13 +9,22 @@ let player ;    // player turn tracking//
 let color ;     // player coloers by swtiching //
 let winner;     //  winnwer player tracking //
 let reset ;      //rest and start again the game//
+let total = 0;    
 
-
+let grid = [
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+]
 
 /*----- cached element references -----*/
 
 const grid = document.querySelectorAll('cell');
 const startEl = document.querySelector('reset-button');
+
 
 
 
