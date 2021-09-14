cba028cd3f0c7aef0a60de17712e5bf466489be3
Latif Basham <latifbasham@Latifs-MacBook-Pro.local>
2021-09-14 10:42:36 -0700

    my car

--- js/main.js
+++ js/main.js
@@ -14,18 +14,19 @@ let total = 0;
 // winning Array by each row and culomns //
 
 let grid = [
-    [0,1,2,3,4,5,6],
-    [7,8,9, 10,11,12,13],
-    [14,15,16,17,18,19,20],
-    [21,22,23,24,25,26,27],
-    [28,29,30,31,32,33,34],
-    [35,36,37,38,39,40,41],
+    [null,null,null,null,null,null,null],
+    [null,null,null,null,null,null,null],
+    [null,null,null,null,null,null,null],
+    [null,null,null,null,null,null,null],
+    [null,null,null,null,null,null,null],
+    [null,null,null,null,null,null,null],
 ]
 
 /*----- cached element references -----*/
+
 const board= document.querySelector('.board') 
 const cell= document.querySelectorAll('.cell');
-const startEl = document.querySelector('reset-button');
+const startEl = document.querySelector('.start');
 
 console.log(cell)
 
@@ -33,7 +34,21 @@ console.log(cell)
 
 board.addEventListener('click', function(e){
     console.log(e.target.id)
+    let idx=Number(e.target.id)
+    let y=idx%7
+    let x=Math.floor(idx/7)
+    console.log(x,y)
+    grid[x][y]='red'
+    console.log(grid)
+    render(idx)
+})
+startEl.addEventListener('click',function(e){
+    console.log(e.target)
 })
 
-
 /*----- functions -----*/
+
+function render(idx){
+    cell[idx].style.backgroundColor='red'
+    // cell[idx].style
+}
