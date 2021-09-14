efaa09aa517389d71eac89e0391f21ba8c61f22d
Latif Basham <latifbasham@Latifs-MacBook-Pro.local>
2021-09-13 20:26:27 -0700

    my car

--- js/main.js
+++ js/main.js
@@ -17,11 +17,11 @@ let grid = [
     [15,16,17,18,19,20,21],
     [22,23,24,25,26,27,28],
     [29,30,31,32,33,34,34],
-    [35,36,37,38,39,40,42],
+    [35,36,37,38,39,40,41],
 ]
 
 /*----- cached element references -----*/
-const board= document.querySelector('.board')  
+const board= document.querySelector('.board') 
 const cell= document.querySelectorAll('.cell');
 const startEl = document.querySelector('reset-button');
 
@@ -32,8 +32,6 @@ console.log(cell)
 board.addEventListener('click', function(e){
     console.log(e.target.id)
 })
-
-
 
 
 /*----- functions -----*/
